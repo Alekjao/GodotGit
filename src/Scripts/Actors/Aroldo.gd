a8aad6c5aa91fa1b181ed627cb8ae40bef6ac2eb
Alekjao <69696943+Alekjao@users.noreply.github.com>
2020-08-16 20:22:03 -0300

    dawdsd

--- src/Scripts/Actors/Aroldo.gd
+++ src/Scripts/Actors/Aroldo.gd
@@ -12,9 +12,11 @@ var next_dir_time = 0
 func _ready():
 	set_process(true)
 
-var direction: = get_direction()
 
-
+#func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0 
+	var direction: = get_direction()
+#	_velocity = calculate_move_velocity(_velocity,speed,direction,is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 func get_direction() -> Vector2:
 	if Player.position.x < position.x and next_dir != -1:
